b58d9b3b09a6c9c58bbe3c13b3c04f1b3d058a92
William Petit <w.petit@ed.ac.uk>
2019-02-28 17:14:22 +0000

    Work - 28/02/2019

--- main/scene/Ball.cs
+++ main/scene/Ball.cs
@@ -1,9 +1,10 @@
 using Godot;
 using System;
+using System.Collections;
 
 public class Ball : RigidBody2D
 {
-    public Vector2 direction = new Vector2(0.80f, -1.0f);
+    public Vector2 direction = new Vector2(0.80f, 1.0f);
 
     // Constant for ball speed (in pixels/second)
     public const int INITIAL_BALL_SPEED = 200;
@@ -15,6 +16,7 @@ public class Ball : RigidBody2D
 
     public override void _Ready()
     {
+        Console.WriteLine("Hello World!");
         court_size = GetViewport().GetSize();
         random = new Random();
     }
@@ -27,13 +29,29 @@ public class Ball : RigidBody2D
         if ((Position.x < 0 && direction.x < 0) || (Position.x > court_size.x && direction.x > 0)){
             direction.x = -direction.x;
         }
+        
+        Godot.Array bodies = this.GetCollidingBodies();
+        IEnumerator enumerator = bodies.GetEnumerator();
+
+        while(enumerator.MoveNext()){
+            Console.WriteLine("Contact!");
+
+            Node body = (Node)enumerator.Current;
+
+            if( body.IsInGroup("pads") ){
+                direction.y = -direction.y;
+                direction.x = -direction.x;
+                direction = direction.Normalized();
+                ballSpeed *= 1.1f;
+            }
+        }
     }
 
-    public void OnBallBodyEntered(Godot.Object body){
+    public void _on_Ball_body_entered(){
         direction.y = -direction.y;
-        direction.x = NextFloat(random);
+        direction.x = -direction.x;
         direction = direction.Normalized();
-        ((Ball)GetNode("Ball")).ballSpeed *= 1.1f;
+        ballSpeed *= 1.1f;
     }
 
     static float NextFloat(Random random)
